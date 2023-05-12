650d1a3c2b080b421e10edcbfb47624928c51d57
MusicShaun <music.shaunpickett@gmail.com>
2023-05-12 12:43:25 +0800

    Skills Page restyling

--- src/features/skills/Skills.js
+++ src/features/skills/Skills.js
@@ -15,11 +15,11 @@ export default function Skills() {
   }, [])
 
 
-  const DARKBLUE = '#90ABD1';
-  const ORANGE = '#F4A86A';
+  const DARKBLUE = 'hsl(215,58%,79%)';
+  const ORANGE = 'hsl(27,86%,76%)';
   const YELLOW = 'hsl(50,98%,76%)';
   const LIGHTBLUE = 'hsl(193,60%,78%)';
-  const LIGHTORANGE = '#F2BC8D';
+  const LIGHTORANGE = 'hsl(28,90%,80%)';
 
   const SKILLSBOX = {
     heading: 'Skills',
@@ -30,35 +30,63 @@ export default function Skills() {
       ['HTML', 'CSS', 'JAVASCRIPT', 'NodeJS'],
       ['React', 'Vue'],
       ['RTK-Query', 'NextJS'],
-      ['MongoDb','RESTful API\'s', 'Typescript', 'RTK-Query' ,'Responsive Design'],
+      ['MongoDb','RESTful API\'s', 'Typescript' ,'Responsive Design'],
       ['']
     ],
   }
   const INTROBOX = {
-    heading: 'SHAUN PICKETT',
-    subHeading: [''],
-    list: [
-      ['music.ShaunPickett@gmail.com'],
-      ['0412 220 332'], ['Trigg, Perth, Wa 6029'],
-      ['www.ShaunPickett.com']
+    subHeading: ['PROFESSIONAL SUMMARY'],
+    list: [`        Logical and results-driven Web Developer dedicated to building and optimizing user-focused 
+    websites for customers with various business objectives. Judicious and creative when crafting
+     effective websites, apps and platforms to propel competitive advantage and revenue growth.
+      Technically proficient and analytical problem solver with calm and focused demeanor.`
     ],
+  }
+  const ABOUTBOX = {
+    subHeading: [
+      'PROFESSIONAL EXPERIENCE'
+    ],
+    title: ['Mix Engineer & Mastering Engineer, Song Writer', 'Helena College - Teacher '],
+    date: ['01/2011 - 01/2022', '01/2014 - 01/2017'],
+    list: [
+      ['Collaborated with producers and performers to create and brainstorm new ideas and works.',
+        'Toured as a performer.',
+        'Mixed and mastered vocal arrangements and music compositions, and optimized songs and albums.',
+        'Used Ableton and hardware to mix and edit tracks.'],
+      ['Worked cooperatively with other teachers, administrators and parents to help students reach learning objectives.',
+      'Communicated frequently with parents, students and faculty to provide feedback and discuss instructional strategies.',
+      'Kept students on-task with proactive behavior modification and positive reinforcement strategies.',
+      'Incorporated multiple types of teaching strategies into classroom.',
+        'Planned and implemented integrated lessons to meet national standards.'],
+      ['At Helena College, I achieved a 250% increase in student enrollment by the third year of employment',
+      'As an audio engineer I maintained consistent music clientele from around the world by repeatedly producing exceptional results and collaborating closely with clients to ensure their complete satisfaction',
+      'As an artist and brand, I successfully managed global releases, national touring, marketing, and video productions through careful prioritization of tasks using S.M.A.R.T goals']
+    ]
+  }
+  const LASTBOX = {
+    subHeading: 'ACCOMPLISHMENTS',
+    list: ['At Helena College, I achieved a 250% increase in student enrollment by the third year of employment',
+    'As an audio engineer I maintained consistent music clientele from around the world by repeatedly producing exceptional results and collaborating closely with clients to ensure their complete satisfaction',
+      'As an artist and brand, I successfully managed global releases, national touring, marketing, and video productions through careful prioritization of tasks using S.M.A.R.T goals'
+    ]
   }
 
 
   return (
   <Wrapper>
-            {loading && <Loader /> }
+      {loading && <Loader />}
+      <BoxWrap>
     <Box>
-        <Header>Skills</Header>
+        {/* <Header>Skills</Header> */}
 
 
 
     <Containers style={{ backgroundColor: YELLOW }}>
-      <Headings>{INTROBOX.heading}</Headings>
-        <UL>
+          <UL>
+          <SubHeading>{INTROBOX.subHeading}</SubHeading>
           {INTROBOX.list.map((item, index) => {  // MAP THE LIST ARRAY
           return (
-            <LI><span>{item}</span></LI>
+            <PARA>{item}</PARA>
           )
           })}
         </UL>
@@ -77,56 +105,49 @@ export default function Skills() {
               </UL>
             )
           })}
-      </Containers>
+          </Containers>
+          
+      <Containers style={{ backgroundColor: ORANGE }}>
+      <DownloadBtn>Download Resume as PDF</DownloadBtn>
+          <PDFPreview></PDFPreview> 
 
-      <Containers style={{backgroundColor: DARKBLUE}}>
-        <Headings>Work Descriptions    </Headings>
-          <SubHeading>Music Teacher </SubHeading>
-        <UL>
-            <LI>Liaising with parents, teachers, principles, children and people with learning difficulties</LI>
-            <LI>Organising concerts and performances</LI>
-            <LI>Creating curriculum for the students</LI>
-            <LI>Safe and inclusive work practices</LI>
-        </UL>
-        <SubHeading>Mastering Engineer</SubHeading>
-        <UL>
-            <LI>Liaising with international artists and working towards a common goal.</LI>
-            <LI>Striving for the best product every time.</LI>
-            <LI>Providing community and mentorship to up and coming artists</LI>
-            <LI>Working with high pressure deadlines and intense environments</LI>
-        </UL>
-          <SubHeading>Producer / Sound Designer / Artist</SubHeading>
-        <UL>
-            <LI>Writing & producing music</LI>
-            <LI>Creating visual content and music videos</LI>
-            <LI>Leading a 4 person team throughout many national tours </LI>
-            <LI>Workshopping audio with actors and lighting</LI>
-            <LI>Being performance ready for every show</LI>
-        </UL>
+      </Containers>
+          
+        </Box>
+        <Box>
+          <Containers style={{ backgroundColor: LIGHTORANGE }}>
+            
+      {ABOUTBOX.title.map((heading, index) => {  //MAP THE SUB HEADINGS 
+            return (
+              <UL>
+                <SubHeading>{ABOUTBOX.subHeading[index]}</SubHeading>
+                <Title style={{marginTop: '1rem'}}>{heading}</Title>
+                <Title style={{marginBottom: '1rem', fontStyle: 'italic'}}>{ABOUTBOX.date[index]}</Title>
+                {ABOUTBOX.list[index].map((item, index) => {  // MAP THE LIST ARRAY
+                return (
+                  <LI><span>{item}</span></LI>
+                )
+              })}
+              </UL>
+            )
+          })}
 
     </Containers>
     
-    <Containers style={{backgroundColor: LIGHTORANGE}}>
-      <Headings>Skills</Headings>
-        <UL>
-          <LI>CSS/SASS</LI>
-          <LI>Javascript/React/Typescript</LI>
-          <LI>Node.js</LI>
-          <LI>RTK - Query</LI>
-          <LI>NextJS</LI>
-          <LI>MUI</LI>
-          <LI>Anything Audio</LI>
-        </UL>
+    <Containers style={{backgroundColor: DARKBLUE}}>
+              <UL>
+                <SubHeading>{LASTBOX.subHeading}</SubHeading>
+                {LASTBOX.list.map((item, index) => {  // MAP THE LIST ARRAY
+                return (
+                  <LI><span>{item}</span></LI>
+                )
+              })}
+              </UL>
     </Containers>
 
-    <Containers style={{backgroundColor: '#F2BC8D'}}>
-      <Headings>Current</Headings>
-        <UL>
-          <LI>Current study focus - Node.JS</LI>
-          <LI>Finishing a MERN stack eCommerce Site</LI>
-        </UL>
-    </Containers>
-    </Box>
+   
+        </Box>
+        </BoxWrap>
   </Wrapper>
   )
 }
@@ -149,15 +170,22 @@ const Wrapper = styled.div`
 
   color:${colorBlack} ;
 `;
+const BoxWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
+`
 const Box = styled.div`
-  width:90%;
+  width:40%;
   height: 100%;
   display: flex;
   flex-direction: column;
-  flex-wrap: wrap;
-  justify-content: flex-start;
+  gap: 2rem;
+  margin-top: 1rem;
+
   ${device.laptopL}{
-    justify-content: center;
+    /* justify-content: center; */
   }
 
 `;
@@ -175,14 +203,15 @@ const Header = styled.div`
   }
 `;
 const Containers = styled.div`
-  width:49%;
-  height: auto;
-  margin: 1rem 0;
   z-index: 1;
   border-radius: 5px;
   padding-bottom: 1rem;
+
+  & ul:first-child {
+    margin-top: 1.5rem;
+  }
   ${device.laptopL}{
-    width:90%;
+    /* width:90%; */
   }
 `;
 const Headings = styled.div`
@@ -199,19 +228,40 @@ const UL = styled.ul`
 `;
 const SubHeading = styled.div`
   font-size: 1.35rem;
-  font-weight: 500;
-  margin-left: 0rem;
+  font-weight: 600;
   color: ${colorDarkGrey};
 `;
+const Title = styled.div`
+  font-size: 0.9rem;
+  font-weight: 600;
+
+`
 const LI = styled.li`
   font-size: 0.5rem;
   padding-right: 0.3rem;
   margin-left: 1rem;
+  padding-bottom: 5px;
 
   & span {
     margin-left: 0.5rem;
-    letter-spacing: 0.7px;
+    letter-spacing: 0.3px;
     font-size: 0.9rem;
+    line-height: 16px;
+
 
   }
 `;
+
+const PARA = styled.p`
+  font-size: 0.9rem;
+  padding-right: 0.3rem;
+  margin-left: 1rem;
+
+`
+const DownloadBtn = styled.button`
+
+
+`
+const PDFPreview = styled.div`
+
+`
