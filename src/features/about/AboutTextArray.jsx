bcec788fe897d1f5b9c413af44270afb695923b7
Shaun Pickett <98673276+MusicShaun@users.noreply.github.com>
2024-07-22 12:47:28 +0800

    Update AboutTextArray.jsx
    Amended the text so it was more relavent and less lame

--- src/features/about/AboutTextArray.jsx
+++ src/features/about/AboutTextArray.jsx
@@ -4,7 +4,7 @@ export const aboutTextArray = [
 
 'I\'ve been lucky enough to travel and collaborate with people from all around the world.',
 
-'My specialty was producing music but people also came to me for my rad mastering skills.', 
+'My specialty was producing music but somehow people found me for my rad mastering skills.', 
 
 'Life was good. Until it wasn\'t.',
 
@@ -14,15 +14,15 @@ export const aboutTextArray = [
 
 'In less than a week, all my income streams had vanished and in case you\'re wondering, musicians aren\'t cashing redundancy payouts.',
 
-'It was time to walk a new path. To find a challenge that I can be passionate about. And that\'s when I found coding. And to my surprise, coding is incredibly satisfying, like finding the right chord progression.',
+'It was time to walk a new path. To find a challenge that I can be passionate about. And that\'s when I found coding and IT.',
 
 'Now I am but a humble Padawan, on a journey to seek wisdom. From bass and beats to bytes and bits.',
 
-'I hope to sit side by side with the javascript jedi\'s, crafting apps and games, solving problems, failing compiles, learning new tools.',
+'I hope to sit side by side with the javascript jedi\'s, building networks, solving problems, failing compiles, learning new tools.',
 
-'I didn\'t begin coding because someone told me to or because I needed a job that pays the big bucks. This field is very enjoyable.',
+'It's a good experience transitioning careers. ',
 
-'It\'s like composing a symphony of logic and creativity, and I\'m the maestro.',
+'I am now on the job hunt. Please don't hesitate to say hello. ',
 
 '',
 
